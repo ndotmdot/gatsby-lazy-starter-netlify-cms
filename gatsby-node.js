677aec558c63724dda71c4540d37e126e45a4e26
Niklas May <hello@niklas-may.de>
2019-04-03 22:57:38 +0200

    cms

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,8 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
+ // relative filepathes
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 exports.onCreateNode = ({ node }) => {
   fmImagesToRelative(node);
 };
+
+// create slugs
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
