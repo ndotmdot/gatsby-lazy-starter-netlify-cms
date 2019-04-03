42bc79c369fa9cee411436f33e23801e68e9fc05
Niklas May <hello@niklas-may.de>
2019-04-03 23:50:44 +0200

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,21 +18,21 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `media`,
-        path: `${__dirname}/src/static/content/media`,
+        path: `${__dirname}/static/content/media`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/static/content/pages`,
+        path: `${__dirname}/static/content/pages`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/src/static/content/blog`,
+        path: `${__dirname}/static/content/blog`,
       },
     },
     {
@@ -69,9 +69,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-
-    
+    },    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
