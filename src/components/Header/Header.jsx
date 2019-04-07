0d5c453cf6dc14e0431e2936fe827630479ae24a
Niklas May <hello@niklas-may.de>
2019-04-07 15:30:10 +0200

    changed cms structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,11 +13,12 @@ const Header = ({ siteTitle, menuLinks, location = "isHome" }) => {
       {
         menuLinks.map(link => {
           const isHome = location === "isHome" ? true : false
-          const linkIsCurrentPage = location.pathname === link.link ? true : false
+          const linkIsCurrentPage = location.pathname === link.link ? false : true
+
           if(isHome){
             if(link.link === "/about") {
               return (
-                <div className="col">
+                <div className="col" key={link.link}>
                   <Link to={link.link}><p>{link.name}</p></Link>
                 </div>
               )
@@ -25,13 +26,13 @@ const Header = ({ siteTitle, menuLinks, location = "isHome" }) => {
           } else {
             if(linkIsCurrentPage){
               return (
-                <div className="col">
+                <div className="col" key={link.link}>
                   <Link to={link.link}><p>{link.name}</p></Link>
                 </div>
               )
             }
           }
-          return <div />
+          return <div key={link.link}/>
         })
       }
     </RowFixed>
